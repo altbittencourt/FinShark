e4d04fac99087b462dbc99a492c08953eecc7eda
altbittencourt <altbittencourt@gmail.com>
2024-10-01 15:36:12 -0300

    Adding Stock controller

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -13,7 +13,7 @@ namespace api.Data
         {
             
         }
-        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
     }
 }
