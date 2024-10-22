5d797568757a005d0c278dcc43c493be0e0747e3
altbittencourt <altbittencourt@gmail.com>
2024-10-21 22:03:17 -0300

    Adding stock query filter

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using api.Models;
@@ -21,9 +22,21 @@ namespace api.Repository
 			_context = context;
 		}
 
-		public async Task<List<Stock>> GetAllAsync()
+		public async Task<List<Stock>> GetAllAsync(QueryObject query)
 		{
-			return await _context.Stocks.Include(x => x.Comments).ToListAsync();
+			var stocks = _context.Stocks.Include(x => x.Comments).AsQueryable();
+
+			if (!string.IsNullOrWhiteSpace(query.CompanyName))
+			{
+				stocks = stocks.Where(x => x.CompanyName.Contains(query.CompanyName));
+			}
+
+			if (!string.IsNullOrWhiteSpace(query.Symbol))
+			{
+				stocks = stocks.Where(x => x.Symbol.Contains(query.Symbol));
+			}
+
+			return await stocks.ToListAsync();
 		}
 
 		public async Task<Stock?> GetByIdAsync(int id)
